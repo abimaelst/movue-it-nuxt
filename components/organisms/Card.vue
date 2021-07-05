bed9a7ba2936f870fcbc3fb2bd0b88b2b9efa986
abimaelst <abimaelst@live.com>
2021-07-05 01:11:05 -0300

    create card to show finish chanllenge

--- components/organisms/Card.vue
+++ components/organisms/Card.vue
@@ -1,9 +1,17 @@
 <template>
-  <h1>Hello</h1>
+  <div class="bg-white rounded-md flex flex-1 w-full mt-8 shadow-sm lg:mt-0 ">
+    <Challenge v-if="currentChallenge" v-bind="currentChallenge"/>
+    <StartCycle v-else />
+    <LevelUpModal />
+  </div>
 </template>
 <script lang="ts">
 import Vue from 'vue'
+import { mapGetters } from 'vuex'
+
 export default Vue.extend({
-  
+computed: {
+  ...mapGetters('Challenges', ['currentChallenge'])
+}
 })
 </script>
