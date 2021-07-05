7f0dcaa9cb4398f853fa9996adac43d11628764f
abimaelst <abimaelst@live.com>
2021-07-04 22:52:50 -0300

    Create Profile User

---
+++ components/molecules/Profile.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex items-center">
+    <ProfileImg />
+    <ProfileInfo :current-level="level" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { mapState } from 'vuex'
+
+export default Vue.extend({
+  computed: mapState('Challenges', ['level'])
+})
+</script>
