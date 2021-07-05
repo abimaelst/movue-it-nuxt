f3cb13ab74b92ae2c0f7a98134f66bf5dfd8eb59
abimaelst <abimaelst@live.com>
2021-07-04 22:52:14 -0300

    Create el to info profile

---
+++ components/atoms/ProfileInfo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex flex-col w-full">
+      <strong class="text-title text-2xl">
+        Abimael Tavares
+      </strong>
+        <p class="flex items-center">
+          <img src="icons/level.svg" alt="Ìcone simbolizando level" class="mr-2">
+          <span class="text-base"> {{ `Level ${currentLevel}` }}</span>
+        </p>
+    </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    currentLevel: {
+    type: Number,
+    required: true
+    }
+  }
+})
+</script>
