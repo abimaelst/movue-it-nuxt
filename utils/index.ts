797b4591c68b3edc71022e0cea7a90f040e5a9dd
abimaelst <abimaelst@live.com>
2021-07-05 00:18:43 -0300

    create scroll to element for card

--- utils/index.ts
+++ utils/index.ts
@@ -3,3 +3,16 @@ export const playAudio = (path: string) => new Audio(path).play()
 export const sendNotification = (title: string, options: object) => new Notification(title, options)
 
 export const splitValue = (value: string | number, padSize: number = 2) => `${value}`.padStart(padSize, '0').split('')
+
+export const getRandomNumber = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min) + min)
+}
+
+export const scrollToElement = (selector: string) => {
+  const el: HTMLElement | null  = document.querySelector(selector)
+  const mq = window.matchMedia('(max-width: 693px)')
+
+  if (el && mq.matches) {
+    el.scrollIntoView({block: 'start', behavior: 'smooth'})
+  }
+}
