4e5c56d41c60d9803798af6b553efa9369e60227
abimaelst <abimaelst@live.com>
2021-07-04 22:51:10 -0300

    Create Button Comp

---
+++ components/atoms/Button.vue
@@ -0,0 +1,41 @@
+<template>
+  <button
+    :class="[
+      'button',
+      { completed: completed },
+      { abandon: abandon },
+      { start: start },
+    ]"
+    :disabled="hasActive"
+    @click="$emit('click')"
+  >
+    {{ text }}
+  </button>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    hasActive: {
+      type: Boolean,
+      default: false,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    abandon: {
+      type: Boolean,
+      default: false,
+    },
+    start: {
+      type: Boolean,
+      default: false,
+    },
+  },
+})
+</script>
